29b6a0f7b2f5158e7c4d70e01e64d2dbf5ca320f
Greg Linklater <greglink49@gmail.com>
2018-05-18 13:21:56 +0200

    better find for perlin noise classification

--- src/PerlinGenerator.js
+++ src/PerlinGenerator.js
@@ -30,8 +30,17 @@ class PerlinGenerator {
   }
 
   interpret (levels = [{ level: 0.5, value: 1 }]) {
-    levels = levels.sort((a, b) => a < b ? -1 : a > b ? 1 : 0).reverse()
-    return this.noise.map(float => (levels.find(item => float >= item.level) || {}).value || 0)
+    levels = levels.sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+    return this.noise.map(float => {
+      let value = 0
+      for (let i = levels.length - 1; i >= 0; i--) {
+        if (float >= levels[i].level) {
+          value = levels[i].value
+          break
+        }
+      }
+      return value
+    })
   }
 }
 
